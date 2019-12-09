e128f67271bf6aa75d3c89ddfab80e5920efb535
liangshuai <1589900136@qq.com>
2019-12-09 10:06:41 +0800

    Initial commit

---
+++ Guns/sss.sql
@@ -0,0 +1,75 @@
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129309220866(Long), 105(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129325998082(Long), 119(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129330192385(Long), 120(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129338580993(Long), 121(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129342775297(Long), 122(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129351163906(Long), 150(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129359552513(Long), 151(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129367941122(Long), 128(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129372135425(Long), 134(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129384718338(Long), 158(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129384718339(Long), 159(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129393106946(Long), 130(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129401495554(Long), 133(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129409884162(Long), 160(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129418272769(Long), 161(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129426661377(Long), 171(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129435049985(Long), 149(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129443438593(Long), 1139826657964593154(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129451827202(Long), 1142957882422112257(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129456021505(Long), 1142957882422112258(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129464410113(Long), 1142957882422112259(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129472798721(Long), 1142957882426306562(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129481187329(Long), 172(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129489575937(Long), 1111546189892870145(Long), 6(Long)
+
+INSERT INTO sys_relation ( relation_id, menu_id, role_id ) VALUES ( ?, ?, ? )
+==> Parameters: 1191539129497964545(Long), 1149955324929765378(Long), 6(Long)
