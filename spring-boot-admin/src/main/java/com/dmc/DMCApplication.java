23c8b2cf290b7d17ad8b58d6d80517c1b4129552
liangshuai <1589900136@qq.com>
2020-01-22 17:57:51 +0800

    ss

--- spring-boot-admin/src/main/java/com/dmc/DMCApplication.java
+++ spring-boot-admin/src/main/java/com/dmc/DMCApplication.java
@@ -1,11 +1,7 @@
 package com.dmc;
 
-import com.dmc.util.JsonUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
